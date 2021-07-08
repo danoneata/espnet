1de64c75fd026abaf47a4dae01acc0b3814dffa7
Dan Oneata <dan.oneata@gmail.com>
2021-07-08 23:13:53 +0300

    FIX Squeeze array before PCA

--- egs2/obama/inference_audio.py
+++ egs2/obama/inference_audio.py
@@ -3,6 +3,7 @@ import pickle
 import os
 
 import click
+import numpy as np
 import soundfile
 import torch
 
@@ -39,16 +40,23 @@ def main(path_input, path_output):
     audio2lip = Audio2Lip(
         train_config=PATH_CONFIG,
         model_file=PATH_MODEL,
+        device="cuda",
         fps_video=FPS_VIDEO,
         fps_audio=FPS_AUDIO,
     )
+
     with open(PATH_PCA, "rb") as f:
         pca = pickle.load(f)
+
     audio, rate = soundfile.read(path_input)
     assert rate == FPS_AUDIO
+
     lips_sm = audio2lip(torch.tensor(audio))
-    pdb.set_trace()
+    lips_sm = lips_sm.cpu().numpy().squeeze(0)
+
     lips_lg = pca.inverse_transform(lips_sm)
+    lips_lg = lips_lg.reshape(-1, 20, 2)
+
     np.save(path_output, lips_lg)
 
 
